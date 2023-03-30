299ee734c6d5a0fbff393802051d3584e04dcad3
Ben Thomas <ben.thomas1984@gmail.com>
2023-03-30 16:24:31 +0100

    Strip out example TF code

--- versions.tf
+++ versions.tf
@@ -1,16 +1,12 @@
 # Enforce minimum Terraform and provider version numbers.
 terraform {
   required_providers {
-    #aws = {
-    #  source  = "hashicorp/aws"
-    #  version = "~> 4.45.0"
-    #}
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.4.3"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.45.0"
     }
+
   }
 
-  required_version = ">= 1.1.4"
+  required_version = ">= 1.3.9"
 }
